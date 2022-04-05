f6d3ef6d17a6f5740f7f88e5319cc72a6f488b0e
Crowdbotics <team@crowdbotics.com>
2022-04-05 16:21:29 +0000

    Initial commit.

---
+++ backend/omesi_34604/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for omesi_34604 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'omesi_34604.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "omesi_34604",
+  url: "https://omesi_34604.botics.co",
+  api: "https://omesi_34604.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
